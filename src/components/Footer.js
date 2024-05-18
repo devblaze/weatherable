634a52642853c85c2b115a751ae55e6bd1d4c236
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2024-05-18 21:20:07 +0300

    Fixed the frontend view as close to figma file

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => (
+    <footer className="footer">
+        <p>© Workable Technology Limited 2024</p>
+    </footer>
+);
+
+export default Footer;
--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -2,28 +2,30 @@ import React, { useState } from 'react';
 import '../styles/WeatherForm.css';
 
 const WeatherForm = ({ addCity }) => {
-  const [city, setCity] = useState('');
+    const [city, setCity] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (city.trim()) {
-      addCity(city);
-      setCity('');
-    }
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (city.trim()) {
+            addCity(city);
+            setCity('');
+        }
+    };
 
-  return (
-    <form className="weather-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="Example: New York, United States"
-        className="city-input"
-      />
-      <button type="submit" className="add-city-button">Submit</button>
-    </form>
-  );
+    return (
+        <div className="weather-form-container">
+            <form className="weather-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    placeholder="Example: New York, United States"
+                    className="city-input"
+                />
+                <button type="submit" className="add-city-button">Submit</button>
+            </form>
+        </div>
+    );
 };
 
 export default WeatherForm;
